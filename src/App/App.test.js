4d1485d2204cbf7e742f15c4fd87743d08ab7292
Kevin <409255@student.fontys.nl>
2021-04-01 09:34:53 +0200

    fixed test
    fixed test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -6,3 +6,8 @@ import App from './App';
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });*/
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
