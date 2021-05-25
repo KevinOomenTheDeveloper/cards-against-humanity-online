ca945badcd57b4226a31ff48d7ea109624a0b372
I409255 <409255@student.fontys.nl>
2021-05-25 23:46:48 +0200

    Added axios and database test

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from "react";
 import "./Home.scss"
 import Container from "react-bootstrap/Container";
+import { useKeycloak } from "@react-keycloak/web";
+import axios from "axios";
 
 const Home = () => {
+    const {keycloak} = useKeycloak();
+    const [content, updateContent] = useState("");
+    useEffect( () => {
+        const fetch = async () => 
+        {
+            const config = {
+                headers: {
+                   Authorization: "Bearer " + keycloak.token
+                }
+             }
+            const result = await axios("http://localhost:9012/message/all", config);
+            return result.data;
+        }
+        fetch().then(d => updateContent(JSON.stringify(d))).catch(e => console.log(e));
+    }, [keycloak.token]);
+
     return (
         <Container>
-            test
+            {content}
         </Container>
     );
 };
